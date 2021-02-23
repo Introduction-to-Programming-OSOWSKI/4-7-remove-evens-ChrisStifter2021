7beb826e8baf5fa34243948ddfd348c767615b82
ChrisStifter2021 <814623@mystma.org>
2021-02-23 16:20:12 +0000

    removeevens1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(x):
+    total = x
+    numPoppped = 0
+    for i in range(0, len(x)):
+        if total[i - numPopped] % 2 == 0 and total[ i - numPopped] !=0:
+            total.pop(i - numPopped)
+            numPopped = numPopped + 1
+    return total
+
+print(removeEvens([1,2,3,4,5,6,7,8,]))
